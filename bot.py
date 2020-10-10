e0fd90230cd2bc6254be93820f8902859bccc2cb
paul6ix <okporp@gmail.com>
2020-10-10 12:52:00 +0100

    Installed env package to prevent exposure of API keys Added env files and referenced the api keys
    Signed-off-by: paul6ix <okporp@gmail.com>

--- bot.py
+++ bot.py
@@ -1,6 +1,12 @@
 import tweepy
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
-
+consumer_key = os.environ.get("consumer_key")
+consumer_secret = os.environ.get("consumer_secret")
+access_token = os.environ.get("access_token")
+access_token_secret = os.environ.get("access_token_secret")
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 
@@ -27,8 +33,9 @@ def save_tweet_id(file, ids):
 
 
 save_tweet_id(FILE_NAME, result)
-# tweets = api.home_timeline()
-# for tweet in tweets:
-#     if '#End' in tweet.text:
-#         print('New tweet relating to SARS')
-#         print(str(tweet.id) + ' -- ' + tweet.text)
+
+tweets = api.home_timeline()
+for tweet in tweets:
+    if '#End' in tweet.text:
+        print('New tweet relating to SARS')
+        print(str(tweet.id) + ' -- ' + tweet.text)
